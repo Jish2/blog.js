c6468379a3af240736e2dd3759c0d284ca0cda55
Jish2 <j.j.jappiecraft@gmail.com>
2023-09-18 13:34:10 -0500

    content v0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+	color: turquoise;
+`;
+
+const HeroText = styled.h1`
+	margin: 0;
+	font-size: 72px;
+`;
+
+const PageGeist = styled.div`
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+const HeroGeist = styled(PageGeist)`
+	background-color: var(--primary);
+`;
+
+const Spacer = styled.div`
+	aspect-ratio: 9/2;
+	width: 100%;
+	background-repeat: no-repeat;
+	background-position: center;
+	background-size: cover;
+
+	background-image: url("./wave-seperator.svg");
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<HeroGeist id="hero">
+				<HeroText>blog.js</HeroText>
+				<p>a simple plug & play library for creating blogs</p>
+				<a href="#get-started">
+					<button>get started</button>
+				</a>
+				{/* animated arrow icon please */}
+			</HeroGeist>
+			<Spacer />
+			<PageGeist id="get-started">
+				<HeroText>Hey!</HeroText>
+				<p>thanks for checking us out! blog.js is under development.</p>
+				<p>come back later and see what's happening</p>
+			</PageGeist>
+		</>
+	);
 }
 
-export default App
+export default App;
